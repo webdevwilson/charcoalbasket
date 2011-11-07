036f63720dea578d2494e68fff7baa310f1410a5
Kerry Wilson <kerry@allthingswilson.com>
2011-11-06 22:49:24 -0600

    fixed bug in configuration involving environments

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -14,7 +14,7 @@ class Configuration
     
     def load_file( file )
       @@config ||= YAML.load_file( file )
-      @@config.merge!( @@config['environment'][@@environment] ) if defined?(@@environment) && @@environment && @@config['environment'][@@environment]
+      @@config.deep_merge!( @@config['environment'][@@environment] ) if defined?(@@environment) && @@environment && @@config['environment'][@@environment]
       @@config
     end
     
@@ -31,3 +31,42 @@ class Configuration
   end
   
 end
+
+class Hash
+
+  # Merges self with another hash, recursively.
+  # 
+  # This code was lovingly stolen from some random gem:
+  # http://gemjack.com/gems/tartan-0.1.1/classes/Hash.html
+  # 
+  # Thanks to whoever made it.
+  def deep_merge(hash)
+    target = dup
+    
+    hash.keys.each do |key|
+      if hash[key].is_a? Hash and self[key].is_a? Hash
+        target[key] = target[key].deep_merge(hash[key])
+        next
+      end
+      
+      target[key] = hash[key]
+    end
+    
+    target
+  end
+
+
+  # From: http://www.gemtacular.com/gemdocs/cerberus-0.2.2/doc/classes/Hash.html
+  # File lib/cerberus/utils.rb, line 42
+  def deep_merge!(second)
+    second.each_pair do |k,v|
+      if self[k].is_a?(Hash) and second[k].is_a?(Hash)
+        self[k].deep_merge!(second[k])
+      else
+        self[k] = second[k]
+      end
+    end
+  end
+  
+end
+
